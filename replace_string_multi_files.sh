c95c8b04a2b68319a2ee0376e14c420480863df4
xination <xination@gmail.com>
2018-01-22 21:00:27 -0500

    Update replace_string_multi_files.sh
    avoid using find ... sed -i {}; 
    it will update all the files' modified time even if there is no string1 in it....

--- replace_string_multi_files.sh
+++ replace_string_multi_files.sh
@@ -2,10 +2,20 @@
 # replace string1 to string2 in multiple files.
 # usage ./replace_string_multi_files.sh string1 string2
 
-# to show which file in the current location has the string1
+# to avoid we miss arguments.
+if [ "$1" = "" ] || [ "$2" = "" ]; then
+    echo " we need 2 arguments" 
+    exit
+fi
+
+# we also avoid to change this script.
+thisScript=$0
+thisScript=${thisScript#"./"}
+
+# to show which file in the current location has the string1 in it.
 echo "we have $1 in the following file"
 echo "_______________________________"
-grep -r "$1" * --color 
+grep -r "$1" * --color --exclude=$thisScript 
 echo "_______________________________"
 echo "replace $1 to $2? (y/n)" 
 read ans
@@ -14,7 +24,21 @@ read ans
 
 if [ "$ans" = "y" ];
 then
-    # the string1 and string2 are case sensitive.
-    # but exclude this script.
-    find ./ -type f \( ! -iname "replace*.sh" \) -exec sed -i 's|'"$1"'|'"$2"'|g' {} \;
+
+    # redirect filenames to a temp file.
+    grep -lr "$1" * --exclude=$thisScript > .fileList.txt
+
+    # read in each line and assign it to fileName	
+    # and then we do the substitution. 
+    while read -r fileName; do
+
+	echo "edited $fileName" 
+
+	sed  -i 's|'"$1"'|'"$2"'|g' "${fileName}"
+
+    done < .fileList.txt
+   
+    rm .fileList.txt
 fi
+
+
